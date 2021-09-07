2488ba802eceacf99c1407f356178d97124c68d9
Christine <christine.ng17@gmail.com>
2021-09-07 16:45:54 -0700

    rendering login & register pages in server.js for now

--- frontend/server.js
+++ frontend/server.js
@@ -41,3 +41,14 @@ app.route("/").get((req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+
+//REGISTER PAGE: 
+app.get('/register', (req, res) => {
+  res.render('Register.js')
+})
+
+//LOGIN PAGE: 
+app.get('/login', (req, res) => {
+  res.render('Login.js')
+})
