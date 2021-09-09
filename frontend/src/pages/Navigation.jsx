205daad87fb39d7aba32d4776ead709e99085cf7
Christine <christine.ng17@gmail.com>
2021-09-09 16:26:33 -0700

    minor fixes

--- frontend/src/pages/Navigation.jsx
+++ frontend/src/pages/Navigation.jsx
@@ -6,26 +6,16 @@ function Navigation(props) {
         <div className="navigation">
             <nav class="navbar navbar-expand navbar-dark bg-dark">
                 <div class="container">
-                    <div>
+                    {/* <div> */}
                         <ul class="navbar-nav ml-auto">
-                            <li
-                                class={`nav-item  ${props.location.pathname === "/Register" ? "active" : ""
-                                    }`}
-                            >
-                                <Link class="nav-link" to="/Register">
-                                    Register
-                                </Link>
+                            <li class={`nav-item  ${props.location.pathname === "/Register" ? "active" : ""}`}>
+                                <Link class="nav-link" to="/Register">Register</Link>
                             </li>
-                            <li
-                                class={`nav-item  ${props.location.pathname === "/Login" ? "active" : ""
-                                    }`}
-                            >
-                                <Link class="nav-link" to="/Login">
-                                    Login
-                                </Link>
+                            <li class={`nav-item  ${props.location.pathname === "/Login" ? "active" : ""}`}>
+                                <Link class="nav-link" to="/Login">Login</Link>
                             </li>
                         </ul>
-                    </div>
+                    {/* </div> */}
                 </div>
             </nav>
         </div>
