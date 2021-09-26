d0ebd537a5344b18357723f6d3758aa5880fce7f
Christine <christine.ng17@gmail.com>
2021-09-26 02:00:25 -0700

    login reg css update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,64 +1,121 @@
-import React, {Component} from 'react';
-import './style.css';
-import{withRouter} from "react-router-dom";
+import React, { useContext, useRef } from 'react';
+import { Card, Form, Button, FormGroup } from 'react-bootstrap';
+import { useAuth } from '../config/Authentication'
+import logo from '../Images/google-logo-9824.png';
 
+export default function Login() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    // const {register} = useAuth()
 
-export class Login extends React.Component {
+    // function handleSubmit(e){
+    //     e.preventDefault()
 
-    constructor(props) {
-        super(props);
-    }
+    //     register(emailRef.current.value, passwordRef.current.value)
+    // }
+    return (
+        <>
+            <Card>
+                <Card.Body style={{
+                    color: 'white',
+                    alignItems: 'left'
+                }}>
+                    <h1
+                        style={{
+                            color: 'black',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            textalign: 'left'
+                        }}>
+                        Let's continue what we started
+                    </h1>
+                    <Button type="googleAPI"
+                        style={{
+                            color: '#767575',
+                            background: 'white',
+                            border: '1px solid #1DE19B',
+                            borderRadius: '400px',
+                            padding: '4px 93px',
+                            alignItems: 'right',
+                            height:'35px',
+                            margin: '10px'
+                        }}>
 
-    render() {
-        return (
-            <form> 
-                <div className="base-container" ref={this.props.containerRef}>
-                    <h1>Let's continue what we started</h1>
-                    <div className="content">
-                        <div className="form">
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <input type="text" name="email" placeholder="Email"></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input type="text" name="password" placeholder="Password"></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="sumbit">
-                        <button type="button" className="btn">Login</button>
-                    </div>
-                </div>
-            </form>
-        );
-    }
+                        Login with Google
+                        <img src={logo}
+                            align="right"
+                            width="28px"
+                            height="25px">
+                        </img>
 
+                    </Button>
+                    <p style={{
+                        color: '#767575',
+                        margin: '5px',
+                        fontSize: '15px'
+                    }}>
+                        - OR -
+                    </p>
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Control
+                                type="email"
+                                placeholder="email"
+                                ref={emailRef} required
+                                style={{
+                                    color: 'black',
+                                    background: 'white',
+                                    border: '1px solid #1DE19B',
+                                    borderRadius: '40px',
+                                    padding: '4px 18px',
+                                    alignItems: 'right',
+                                    height:'25px',
+                                    margin: '10px'
+                                }}>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Control
+                                type="password"
+                                placeholder="password"
+                                ref={passwordRef} required
+                                style={{
+                                    color: 'black',
+                                    background: 'white',
+                                    border: '1px solid #1DE19B',
+                                    borderRadius: '40px',
+                                    padding: '4px 18px',
+                                    alignItems: 'right',
+                                    height:'25px',
+                                    margin: '10px'
+                                }}>
+                            </Form.Control>
+                        </Form.Group>
+                        <Button type="loginButton"
+                            style={{
+                                color: 'white',
+                                background: '#1DE19B',
+                                border: '2px solid #19B47C',
+                                borderRadius: '20px',
+                                padding: '6px 18px',
+                                alignItems: 'right',
+                                margin: '10px'
+                            }}>
+                            Login
+                        </Button>
 
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="link" style={{
+                margin: '10px',
+                color: 'black',
+                display: 'flex',
+                flexDirection: 'column',
+                textAlign: 'left'
+            }}>
+                Don't have an account? Register here
+            </div>
+        </>
+    )
 }
-
-// function Login(){
-
-//     return (
-//         <div className="base-container" ref={this.props.containerRef}>
-//             <h1>Login</h1>
-//             <div className="content">
-//                 <div className="form">
-//                     <div className="form-group">
-//                         <label htmlFor="email">Email</label>
-//                         <input type="text" name="email" placeholder="Email"></input>
-//                     </div>
-//                     <div className="form-group">
-//                         <label htmlFor="password">Password</label>
-//                         <input type="text" name="password" placeholder="Password"></input>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className="sumbit">
-//                 <button type="button" className="btn">Login</button>
-//             </div>
-//         </div>
-//     );
-// }
-
-export default Login;
