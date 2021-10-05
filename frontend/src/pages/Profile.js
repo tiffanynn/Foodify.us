94ca9d141575905cb535bc99b651307a755930e1
Christine <christine.ng17@gmail.com>
2021-10-05 02:37:56 -0700

    profile logout button & check to display email used to login

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,12 +1,26 @@
+import Button from "@restart/ui/esm/Button";
 import React from "react";
 import Cards from "./Cards"
 
-function Profile() {
+import { useAuth } from "../config/Authentication.js";
+import { Link, useHistory } from "react-router-dom";
+
+ export default function Profile() {
+  const {logout, currentUser} = useAuth();
+   const history = useHistory();
+  async function handleSubmit(e){
+    await logout();
+    history.push("/login");
+  }
     return (
       <div>
         <Cards />
+        <Button onClick={logout}>
+        Logout
+        </Button>
+        {currentUser && currentUser.email}
       </div>
     );
   }
   
-  export default Profile;
+ 
