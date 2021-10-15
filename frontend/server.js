fe1f872d43cba866e7e1ef99cdc80c1717f31e52
modysleiman <modysleiman@gmail.com>
2021-10-14 20:26:24 -0700

    Register Component now sends new userID and user's Name to backend server upon successful new user creation on the FireBase side

--- frontend/server.js
+++ frontend/server.js
@@ -44,6 +44,7 @@ db.once("open", function () {
 const mainRouter = require("./backend/routes/mainRouter");
 //app.use("/REPLACE ME WITH A SINGLE ROUTE", mainRouter);
 
+/**************** RECIPE/FEED/SEARCH RELATED ROUTES: ***************/
 //Notes:
 // Req.params will contain url slashes
 //SERVER API GET REQUEST EXAMPLE:
@@ -62,18 +63,13 @@ app.route("/feed").get((req, res) => {
       res.send(JSON.parse('{"recipes" : ' + JSON.stringify(recipes) + "}"))
     )
     .catch((err) => res.status(400).json("Error: " + err));
-
-  //Legacy Temporary DB JSON Object
-  //res.send(recipeListData);
 });
 
 // SENDS BACK 1 RECIPE DATA CORRESPONDING TO RECIPE ID
 app.route("/recipe/:recipeID").get((req, res) => {
   console.log("INCOMING RECIPE DATA REQUEST");
   //res.json({ feedExampleData: "hi", example2: "bye" });
-
   var recipeID = req.params.recipeID;
-  //console.log(" RECIPE ID: ", recipeID);
 
   /* QUERIES DB BY RECIPE ID */
   Recipe.find({ _id: recipeID })
@@ -83,69 +79,42 @@ app.route("/recipe/:recipeID").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+/**************** USER/PROFILE RELATED ROUTES: ***************/
+
+//USER SIGNUP, ADDS USER TO USER DB
+// SENDS BACK 1 RECIPE DATA CORRESPONDING TO RECIPE ID
+app.route("/usersignup/:userID/:name").get((req, res) => {
+  var userID = req.params.userID;
+  var name = req.params.name;
+
+  if (userID == null) {
+    res
+      .status(400)
+      .json(
+        "Error: userID is null, new user upload to MongoDB database failed"
+      );
+  }
+  if (name == null) {
+    res
+      .status(400)
+      .json("Error: name is null, new user upload to MongoDB database failed");
+  }
+  console.log(
+    "INCOMING NEW USER POST REQUEST: /usersignup/",
+    req.params.userID,
+    "/",
+    req.params.name
+  );
+
+  /* ENTERS NEW USER IN USERS DB */
+  /*
+  Recipe.find({ _id: recipeID })
+    .then((recipe) =>
+      res.send(JSON.parse('{"recipe" : ' + JSON.stringify(recipe) + "}"))
+    )
+    .catch((err) => res.status(400).json("Error: " + err)); */
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
-
-//LEGACY RECIPE DB FOR DEVELOPMENT
-/*
-const recipeListData = {
-  recipes: [
-    {
-      ID: "1234",
-      Hashtag: "francofoodtips",
-      Title: "Why is it so hard to make cereal ?",
-      Date: "2021-05-18",
-      EstimatedTime: "20",
-      IngredientsList: [
-        "2 cups Cereal Grain",
-        "1 pint of oat milk",
-        "ground wild almond",
-        "flax seeds",
-      ],
-      DietTags: ["pescatarian", "paleo"],
-      Story:
-        "Your eyes crack open, the room is bright. It’s 10 Am, and you’ve slept through all 3 of your alarms. Karen, your mother-in-law will be coming for brunch in just half an hour. You’re feeling anxious, but you have just the right recipe. Cereal. Gently lower your grain of choice into a round bowl. Lather top with fresh milk. Finish with toppings. Serve cold.",
-      ImageURL:
-        "https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/recipes/feaa97ad73e74183b4af84e2fafd8c68.png",
-    },
-    {
-      ID: "1235",
-      Hashtag: "A timeless vegan recipe",
-      Title: "5 tips to spice up your brunch avocado toast",
-      Date: "2021-08-19",
-      EstimatedTime: "10",
-      IngredientsList: [
-        "1 toast",
-        "1/2 avocado",
-        "a dash of pepper & salt",
-        "1 spoon chili flakes",
-        "1 vegan Just Egg",
-      ],
-      DietTags: ["vegan", "pescatarian", "paleo", "gluten free"],
-      Story:
-        "Your eyes crack open, the room is bright. It’s 10 Am, and you’ve slept through all 3 of your alarms. Karen, your mother-in-law will be coming for brunch in just half an hour. You’re feeling anxious, but you have just the right recipe. Cereal. Gently lower your grain of choice into a round bowl. Lather top with fresh milk. Finish with toppings. Serve cold.",
-      ImageURL:
-        "https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/recipes/feaa97ad73e74183b4af84e2fafd8c68.png",
-    },
-    {
-      ID: "1236",
-      Hashtag: "Not authentic but yummy",
-      Title: "A great use for an unused panini maker",
-      Date: "2021-01-18",
-      EstimatedTime: "40",
-      IngredientsList: [
-        "1 tortilla",
-        "1 cup mashed beans",
-        "1/2 avocado",
-        "1/2 cup corn",
-      ],
-      DietTags: ["pescatarian", "paleo", "vegan"],
-      Story:
-        "This is a tortilla that someone put cheese and beans inside of. Then this thing was put in a panini maker. Then it was taken out of the panini maker. After that it was put on a weird grey plate normal people don’t actually eat out of. Top with a squeeze of lemon juice and serve with a side of your favorite avocado-based dipping sauce.",
-      ImageURL:
-        "https://randomwordgenerator.com/img/picture-generator/57e2d64a4a55ab14f1dc8460962e33791c3ad6e04e50744172297cdc9f45c3_640.jpg",
-    },
-  ],
-};
-*/
