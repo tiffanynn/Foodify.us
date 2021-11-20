6e5539a7627054680f5207a6c167087160bffb8f
modysleiman <modysleiman@gmail.com>
2021-11-19 16:25:30 -0800

    added user route to get user data by firebase id

--- frontend/server.js
+++ frontend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const port = process.env.PORT || 5000; //Runs LocalHost Server on Port 5000
 
 app.use(cors());
-app.use(express.json({ limit: '200mb' }));
+app.use(express.json({ limit: "200mb" }));
 
 //maybe helps parsing params issues
 app.set("query parser", "extended");
@@ -33,21 +33,21 @@ const {
   S3Client,
   PutObjectCommand,
   ListBucketsCommand,
-  GetObjectCommand
-} = require('@aws-sdk/client-s3');
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 
 // Set the AWS region
-const REGION = 'us-west-1';
+const REGION = "us-west-1";
 
 // Set the bucket parameters
-const bucketName = 'foodify';
+const bucketName = "foodify";
 const bucketParams = { Bucket: bucketName };
 
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 AWS.config.update({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-    region: 'us-west-1'
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  region: "us-west-1",
 });
 let aws3 = new AWS.S3({ params: { Bucket: bucketName } });
 
@@ -114,50 +114,56 @@ app.route("/recipe/:recipeID").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-app.route("/upload").post( async(req, res) => {
+app.route("/upload").post(async (req, res) => {
   console.log("INCOMING RECIPE IMAGE UPLOAD REQUEST");
 
-  const title = req.body.title
-  const prepTime = req.body.estimatedTime
-  const selectedTags = req.body.selectedTags
-  const hashtag = req.body.hashtag
-  const ingredient = req.body.ingredient
-  const description = req.body.description
+  const title = req.body.title;
+  const prepTime = req.body.estimatedTime;
+  const selectedTags = req.body.selectedTags;
+  const hashtag = req.body.hashtag;
+  const ingredient = req.body.ingredient;
+  const description = req.body.description;
   const date_ob = new Date();
   const date = date_ob.toISOString();
-  const ID = req.body.userName.uid
-  const JSON = {"title" : title, "hashTagList": hashtag, "postDate": date,
-               "estimatedTime": prepTime,"ingredientList": ingredient, 
-               "dietTagList": selectedTags, "story": description, "userName": ID}
+  const ID = req.body.userName.uid;
+  const JSON = {
+    title: title,
+    hashTagList: hashtag,
+    postDate: date,
+    estimatedTime: prepTime,
+    ingredientList: ingredient,
+    dietTagList: selectedTags,
+    story: description,
+    userName: ID,
+  };
 
-  const filetype = req.body.filetype
-  
-  const content = Buffer.from(req.body.data, 'base64');
+  const filetype = req.body.filetype;
 
-  const fileName = 'recipe_pics/' + ID + '/' + date + '.jpeg';
+  const content = Buffer.from(req.body.data, "base64");
+
+  const fileName = "recipe_pics/" + ID + "/" + date + ".jpeg";
   const params = {
-      Bucket: bucketName,
-      Key: fileName,
-      Body: content,
-      ContentType: filetype
+    Bucket: bucketName,
+    Key: fileName,
+    Body: content,
+    ContentType: filetype,
   };
   try {
-      const results = new AWS.S3.ManagedUpload({ params: params });
-      var s3Url
-      results.send(async function(err, data) {
-          s3Url = data.Location
-                // console.log(s3Url)
-          JSON["imgUrl"] = s3Url
-          const collection = db.collection("recipes")
-          const insertResult = await collection.insertOne(JSON)
-          return res.json({ s3Url: data.Location, uploadDate: date });
-      });
-      console.log('Successfully uploaded data to S3');
-
+    const results = new AWS.S3.ManagedUpload({ params: params });
+    var s3Url;
+    results.send(async function (err, data) {
+      s3Url = data.Location;
+      // console.log(s3Url)
+      JSON["imgUrl"] = s3Url;
+      const collection = db.collection("recipes");
+      const insertResult = await collection.insertOne(JSON);
+      return res.json({ s3Url: data.Location, uploadDate: date });
+    });
+    console.log("Successfully uploaded data to S3");
   } catch (err) {
-      console.log('Error', err);
+    console.log("Error", err);
   }
-})
+});
 
 /*
  *
@@ -178,24 +184,59 @@ app.route("/searchbydiet/:dietTag").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+//Retrieved from example on StackOverflow: https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
+const groupParamsByKey = (params) =>
+  [...params.entries()].reduce((acc, tuple) => {
+    // getting the key and value from each tuple
+    const [key, val] = tuple;
+    if (acc.hasOwnProperty(key)) {
+      // if the current key is already an array, we'll add the value to it
+      if (Array.isArray(acc[key])) {
+        acc[key] = [...acc[key], val];
+      } else {
+        // if it's not an array, but contains a value, we'll convert it into an array
+        // and add the current value to it
+        acc[key] = [acc[key], val];
+      }
+    } else {
+      // plain assignment if no special case is present
+      acc[key] = val;
+    }
+
+    return acc;
+  }, {});
+
+function paramsToObject(entries) {
+  const result = {};
+  for (const [key, value] of entries) {
+    // each 'entry' is a [key, value] tupple
+    result[key] = value;
+  }
+  return result;
+}
+
 //SENDS BACK ALL RECIPES MATCHING SEARCH OR HASHTAG
 app.route("/search/:filter").get((req, res) => {
-  let parsedUrl = url.parse(req.params);
-  let parsedQs = querystring.parse(parsedUrl.query);
+  console.log("REQ PARAMS: ", req.params.filter);
+  let urlParams = new URLSearchParams(req.params.filter);
+  console.log("URL PARAMS: ", urlParams);
 
-  // var filter = req.params.filter;
-  // var filterQuery = req.query;
-  console.log("HERE", parsedQs);
-  // console.log("HERE filterQuery", filterQuery.query);
-  // console.log("HERE dietTags", filterQuery.dietTags);
-  //var dietTags = req.params.dietTags;
-  // console.log("PARAMS: ", req.params);
-  // console.log("INCOMING FEED REQUEST", filter, "DIET TAGS: ");
-  // for (let i = 0; i < filter.length(); i++) {
-  //   if
-  // }
-  // var searchWord =
+  //get parameters as key value pairs
+  for (var param of urlParams.entries()) {
+    //console.log("\nkey: " + param[0] + "     value: " + param[1]);
+  }
+
+  //compile parameters into json object:
+  const result = {};
+  for (var param of urlParams.entries()) {
+    // each 'entry' is a [key, value] tupple
+    result[param[0]] = param[1];
+  }
+
+  console.log("FINAL RESULT OBJECT: ", result);
+
   //Identifies if query is hashtag search or normal search
+  let filter = req.params.filter;
   if (filter.substring(0, 1) == "#") {
     console.log("HASHTAG QUERY", filter);
     /* QUERIES DB FOR MATCHING HASHTAG*/
@@ -234,6 +275,21 @@ app.route("/user").get((req, res) => {
     )
     .catch((err) => res.status(400).json("Error: " + err));
 });
+
+//GET 1 USER BY USERID
+app.route("/user/:userid").get((req, res) => {
+  console.log("INCOMING REQUEST FOR USER WITH USERID: ", req.params.userid);
+  var queryUserId = req.params.userid;
+
+  console.log(queryUserId);
+  //QUERY DB FOR USER ID DATA:
+  User.find({ userId: queryUserId })
+    .then((user) =>
+      res.send(JSON.parse('{"user" : ' + JSON.stringify(user) + "}"))
+    )
+    .catch((err) => res.status(400).json("User Query Error: " + err));
+});
+
 //ADD GET USER BY USERNAME
 
 //ADD EDIT USER BY USERNAME AND SECURED USER ID
