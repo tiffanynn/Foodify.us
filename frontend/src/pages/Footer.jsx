fe66dcf64131a11ac241bdd4cbe8b75008b28373
Christine <christine.ng17@gmail.com>
2021-11-23 12:08:36 -0800

    Fixed Footer CSS

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 function Footer(){
     return(
         <div className="footer">
-            <footer class="py-3 bg-darl fixed-bottom">
+            <footer class="py-3 bg-darl fixed-bottom" 
+                style={{ 
+                position: 'relative',
+                bottom: '0'
+                }}
+            >
                     <p class="m-3 text-center">
-                    Copyright © 2021 Foodify us.     Foodify and related marks are registered trademarks of Foodify.
+                    Copyright © 2021 Foodify.us      Foodify and related marks are registered trademarks of Foodify.
                     </p>
             </footer>
         </div>
